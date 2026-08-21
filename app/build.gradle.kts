4774ee1be6b2b366c9f7520d6114ab52322d1198
gavinsunquestor <gavin@sunquestor.com>
2026-08-21 17:47:32 +0800

    fix: 名称角标再微调↑2dp→2dp(上收8dp,水平8dp);1.9.5/code 52

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.mylauncher"
         minSdk = 26
         targetSdk = 35
-        versionCode = 51
-        versionName = "1.9.4"
+        versionCode = 52
+        versionName = "1.9.5"
     }
 
     signingConfigs {
